8630efcc215c7442534f90ed00a378b48eae6e87
Binada-SH <binadasasmika@gmail.com>
2025-05-08 14:26:25 +0530

    Navbar Creating And Implementing

--- coffee_shop/Coffee_Shop/src/App.jsx
+++ coffee_shop/Coffee_Shop/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './App.css'; 
+import Navbar from './Components/navbar/navbar';
 
 const App = () => {
   return (
     <div className= 'container'>
+      <div className="nav">
+        <Navbar/>
+      </div>
       <div className= 'hero_title'>
         <h1>Discover the <br/>Taste of Coffee <br/>at Brownie</h1>
       </div>
