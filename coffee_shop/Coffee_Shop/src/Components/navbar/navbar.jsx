356fc756bc9a36898cd89119a13a4daeb24014ad
Binada-SH <binadasasmika@gmail.com>
2025-05-08 16:00:43 +0530

    Logo Adding For The Navbar

--- coffee_shop/Coffee_Shop/src/Components/navbar/navbar.jsx
+++ coffee_shop/Coffee_Shop/src/Components/navbar/navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import './navbar.css'
+import React from 'react';
+import './navbar.css';
 
 const navbar = () => {
   return (
     <div className= 'navbar'>
+      <div className="logo">
+              <h2>Brownie</h2>
+      </div>
         <ul>
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
