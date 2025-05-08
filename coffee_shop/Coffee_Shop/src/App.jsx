7fcad6d1b9a7f5746a3eba24b5efe2cdbe6b00a4
Binada-SH <binadasasmika@gmail.com>
2025-05-08 17:47:23 +0530

    Website Background Image Scroll Issue Solved

--- coffee_shop/Coffee_Shop/src/App.jsx
+++ coffee_shop/Coffee_Shop/src/App.jsx
@@ -2,20 +2,27 @@ import React from 'react';
 import './App.css'; 
 import Navbar from './Components/navbar/navbar';
 import Cta_btn from './Components/cta_btn/cta_btn';
+import About from './Components/about/about';
+import './assets/coffee.jpg';
 
 const App = () => {
   return (
-    <div className= 'container'>
-      <div className="nav">
-        <Navbar/>
-      </div>
-      <div className= 'hero_title'>
+    <div className= 'app'>
+      <section className= 'hero'>
+      <Navbar/>
+        <div className= 'hero_title'>
         <h1>Discover the <br/>Taste of Coffee <br/>at Brownie</h1>
       </div>
-      <div className="cta">
+      <div className= "cta">
         <Cta_btn/>
       </div>
+    </section>
+
+    <section className= "about">
+        <About/>
+      </section>
     </div>
+    
     
   )
 }
