8560d9d736b49ae185d23bdf63d81d712347ca05
Binada-SH <binadasasmika@gmail.com>
2025-05-08 21:21:01 +0530

    Menu Component Content Adding

---
+++ coffee_shop/Coffee_Shop/src/Components/menu/menu.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const menu = () => {
+  return (
+    <div className='menu'>
+        <div className="menu_title">
+            <h1>Our Menu</h1>
+        </div>
+        <div><p></p></div>
+      
+    </div>
+  )
+}
+
+export default menu
