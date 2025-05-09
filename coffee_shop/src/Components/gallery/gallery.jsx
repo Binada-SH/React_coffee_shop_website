e739a3eb7d32978f35483f53589fa7ba4da69ee0
Binada-SH <binadasasmika@gmail.com>
2025-05-09 16:53:00 +0530

    Gallery Title And Description Adding

--- coffee_shop/src/Components/gallery/gallery.jsx
+++ coffee_shop/src/Components/gallery/gallery.jsx
@@ -4,7 +4,16 @@ import './gallery.css'
 const gallery = () => {
   return (
     <div className='gallery_container'>
-        
+        <div className="gallery_title">
+            <h2>Gallery</h2>
+        </div>
+        <hr className="gline" />
+        <div className="gallery_context">
+            <p>
+                Take a peek at our cozy space, latte art, and fresh bakes!
+            </p>
+        </div>
+
       
     </div>
   )
