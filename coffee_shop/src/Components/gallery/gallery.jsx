b04fd69e1374d648422dac9069cd11be5001ff2b
Binada-SH <binadasasmika@gmail.com>
2025-05-09 16:43:59 +0530

    Gallery Initialization

---
+++ coffee_shop/src/Components/gallery/gallery.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './gallery.css'
+
+const gallery = () => {
+  return (
+    <div className='gallery_container'>
+        
+      
+    </div>
+  )
+}
+
+export default gallery
