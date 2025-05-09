35b1d6c0e427866cc303c3829c9e59a0b59f27e0
Binada-SH <binadasasmika@gmail.com>
2025-05-09 12:26:10 +0530

    Vite Config Repo Name Update

--- coffee_shop/vite.config.js
+++ coffee_shop/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/coffee_shop/',
+  base: '/React_coffee_shop_website/',
   plugins: [react()],
 })
