745374a139db980458155f354fe0d85f9a3e11e9
Binada-SH <binadasasmika@gmail.com>
2025-05-09 17:43:44 +0530

    Image Grid Creating

--- coffee_shop/src/Components/gallery/gallery.jsx
+++ coffee_shop/src/Components/gallery/gallery.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './gallery.css';
-impo
+import Img1 from '../../assets/img1.jpg';
+import Img2 from '../../assets/img2.jpeg';
+import Img3 from '../../assets/img3.jpg';
 
-const images = []
+const images = [Img1,Img2,Img3];
 
 const gallery = () => {
   return (
@@ -16,10 +18,15 @@ const gallery = () => {
                 Take a peek at our cozy space, latte art, and fresh bakes!
             </p>
         </div>
+        <div className="glr_grd">
+            {images.map((src, index)=> (
+                <img key={index} src={src} alt={'img-${index}'} className='grid_item' />
+            ))}
+        </div>
 
       
     </div>
-  )
-}
+  );
+};
 
 export default gallery
