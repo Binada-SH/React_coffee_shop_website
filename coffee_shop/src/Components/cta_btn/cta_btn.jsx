09df66cb95abb85df9a1359202632df998ae8ecf
Binada-SH <binadasasmika@gmail.com>
2025-05-10 12:31:48 +0530

    Call To Action Button Responsive iPhone

--- coffee_shop/src/Components/cta_btn/cta_btn.jsx
+++ coffee_shop/src/Components/cta_btn/cta_btn.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
+import React from 'react';
 import './cta_btn.css';
 
 const cta_btn = () => {
   return (
     <div className= 'cta_btn'>
       <a href="#visitus">Visit Us →</a>
-        
-      
     </div>
   )
 }
