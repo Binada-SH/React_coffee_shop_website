b6e145bdde0d1201311a6c360b97fd953dfcea13
Binada-SH <binadasasmika@gmail.com>
2025-05-10 12:16:51 +0530

    Navbar Contact Linking

--- coffee_shop/src/Components/navbar/navbar.jsx
+++ coffee_shop/src/Components/navbar/navbar.jsx
@@ -12,7 +12,7 @@ const navbar = () => {
             <li><a href="#about">About</a></li>
             <li><a href="#menu">Menu</a></li>
             <li><a href="#gallery">Gallery</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="#visitus">Contact</a></li>
         </ul>
       
     </div>
