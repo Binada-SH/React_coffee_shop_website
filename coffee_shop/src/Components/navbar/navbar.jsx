8d9018c31cbb7963c62e69774bf5d4bbf4014a56
Binada-SH <binadasasmika@gmail.com>
2025-05-11 14:59:51 +0530

    Navbar Toggle Implimentation On Mobile Phones

--- coffee_shop/src/Components/navbar/navbar.jsx
+++ coffee_shop/src/Components/navbar/navbar.jsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 
-const navbar = () => {
-  return (
-    <div className= 'navbar'>
-      <div className="logo">
-              <h2>Brownie</h2>
-      </div>
-        <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#menu">Menu</a></li>
-            <li><a href="#gallery">Gallery</a></li>
-            <li><a href="#visitus">Contact</a></li>
-        </ul>
-      
-    </div>
-  )
-}
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-export default navbar
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <h2>Brownie</h2>
+      </div>
+      
+      <div className={`menu-toggle ${menuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      
+      <ul className={menuOpen ? 'active' : ''}>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#menu">Menu</a></li>
+        <li><a href="#gallery">Gallery</a></li>
+        <li><a href="#visitus">Contact</a></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
